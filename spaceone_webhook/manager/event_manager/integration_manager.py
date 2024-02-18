96beca8abbb532ba095bfb45d9d750034f5abf09
knyg99 <knyg99@naver.com>
2024-02-18 16:51:42 +0900

    feat: integration manager

--- spaceone_webhook/manager/event_manager/integration_manager.py
+++ spaceone_webhook/manager/event_manager/integration_manager.py
@@ -1,14 +1,12 @@
 import logging
 import json
-from spaceone.core.manager import BaseManager
-from spaceone.inventory.plugin.collector.lib import *
 from spaceone_webhook.manager.event_manager.base import ParseManager
 
 _LOGGER = logging.getLogger("spaceone")
 
 
 class IntegrationManager(ParseManager):
-    webhook_type = "Ncloud_Integration"
+    webhook_type = "Integration"
 
     def parse(self, raw_data: dict) -> dict:
         """
@@ -29,7 +27,7 @@ class IntegrationManager(ParseManager):
             'severity': self.get_severity(raw_data),
             #'resource': self._get_resource(raw_data),#not required
             'title': self._change_string_format(raw_data),
-            'rule': raw_data.get("ruleName"),#not required
+            #'rule': raw_data.get("ruleName"),#not required
             'occurred_at': self.convert_to_iso8601(raw_data.get("startTime")),
             'additional_info': self.get_additional_info(raw_data)
         }
@@ -50,8 +48,8 @@ class IntegrationManager(ParseManager):
     def get_severity(self, raw_data: dict) -> str:
         if raw_data.get("eventLevel") is not None:
             return raw_data.get("eventLevel")
-        else :
-            return "UNKNOWN"
+        else:
+            return "INFO"
 
     @staticmethod
     def _change_string_format(raw_data):
@@ -59,25 +57,41 @@ class IntegrationManager(ParseManager):
 
     @staticmethod
     def get_additional_info(raw_data: dict) -> dict:
+        values = {
+            "#{DOMAIN_CODE}": "PUB",
+            "#{RULE_NAME}": "rule002",
+            "#{EVENT_STATUS}": "ALARM",
+            "#{RESOURCE_NAME}": "svr-pub",
+            "#{DIMENSIONS}": "instanceNo: xxxxxxx, type: svr",
+            "#{METRIC}": "avg_write_cnt",
+            "#{OPERATOR}": ">=",
+            "#{CRITERIA}": "0.8",
+            "#{VALUE}": "0.7944445"
+        }
+
         all_keys = [
             "DOMAIN_CODE", "REGION_CODE", "PRODUCT_KEY", "PRODUCT_NAME",
-            "RESOURCE_NAME", "DIMENSIONS", "RULE_ID", "RULE_NAME", "DATA_TIME",
+            "DIMENSIONS", "RULE_ID", "RULE_NAME", "DATA_TIME",
             "LEVEL", "OPERATOR", "METRIC", "UNIT", "AGGREGATION_METHOD",
             "CRITERIA", "DURATION", "ALARM_START_TIME", "ALARM_END_TIME",
             "EVENT_CAUSE_TYPE", "VALUE", "CURRENT_VALUE", "EVENT_STATUS"
         ]
 
         additional_info = {}
+        payload = raw_data.get("payload")
+        for key, value in values.items():
+            payload = payload.replace(key, str(value))
 
-        if "DIMENSIONS" in raw_data and raw_data.get("DIMENSIONS"):
+
+        if "DIMENSIONS" in payload and payload.get("DIMENSIONS"):
             try:
-                dimensions = raw_data["DIMENSIONS"]
+                dimensions = payload["DIMENSIONS"]
                 additional_info["DIMENSIONS"] = dimensions
             except json.JSONDecodeError:
                 additional_info["DIMENSIONS"] = raw_data["DIMENSIONS"]
 
         for key in all_keys:
-            if key in raw_data and key != "DIMENSIONS":
+            if key in payload and key != "DIMENSIONS":
                 additional_info[key] = raw_data[key]
 
         return additional_info
@@ -85,6 +99,10 @@ class IntegrationManager(ParseManager):
 
     @staticmethod
     def _get_resource(raw_data: dict) -> dict:
-        return {
-            "name": raw_data.get("resourceName")
-        }
+        key = ["RESOURCE_NAME"]
+        if key in raw_data:
+            return {
+                    "name" : raw_data[key]
+            }
+        else:
+            return {"name": "Undefined"}
