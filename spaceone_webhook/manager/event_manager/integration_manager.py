cc3cb6c4e4a918f9ab72d8dc9fd5e36d3e56a774
knyg99 <knyg99@naver.com>
2024-02-27 22:28:15 +0900

    feat: add description

--- spaceone_webhook/manager/event_manager/integration_manager.py
+++ spaceone_webhook/manager/event_manager/integration_manager.py
@@ -28,6 +28,7 @@ class IntegrationManager(ParseManager):
             #'resource': self._get_resource(raw_data),#not required
             'title': self._change_string_format(raw_data),
             #'rule': raw_data.get("ruleName"),#not required
+            'description': raw_data.get('payload'),
             'occurred_at': self.convert_to_iso8601(raw_data.get("startTime")),
             'additional_info': self.get_additional_info(raw_data)
         }
@@ -57,42 +58,9 @@ class IntegrationManager(ParseManager):
 
     @staticmethod
     def get_additional_info(raw_data: dict) -> dict:
-        values = {
-            "#{DOMAIN_CODE}": "PUB",
-            "#{RULE_NAME}": "rule002",
-            "#{EVENT_STATUS}": "ALARM",
-            "#{RESOURCE_NAME}": "svr-pub",
-            "#{DIMENSIONS}": "instanceNo: xxxxxxx, type: svr",
-            "#{METRIC}": "avg_write_cnt",
-            "#{OPERATOR}": ">=",
-            "#{CRITERIA}": "0.8",
-            "#{VALUE}": "0.7944445"
+        additional_info = {
+            "url": raw_data.get("url")
         }
-
-        all_keys = [
-            "DOMAIN_CODE", "REGION_CODE", "PRODUCT_KEY", "PRODUCT_NAME",
-            "DIMENSIONS", "RULE_ID", "RULE_NAME", "DATA_TIME",
-            "LEVEL", "OPERATOR", "METRIC", "UNIT", "AGGREGATION_METHOD",
-            "CRITERIA", "DURATION", "ALARM_START_TIME", "ALARM_END_TIME",
-            "EVENT_CAUSE_TYPE", "VALUE", "CURRENT_VALUE", "EVENT_STATUS"
-        ]
-
-        additional_info = {}
-        payload = raw_data.get("payload")
-        for key, value in values.items():
-            payload = payload.replace(key, str(value))
-
-
-        if "DIMENSIONS" in payload and payload.get("DIMENSIONS"):
-            try:
-                dimensions = payload["DIMENSIONS"]
-                additional_info["DIMENSIONS"] = dimensions
-            except json.JSONDecodeError:
-                additional_info["DIMENSIONS"] = raw_data["DIMENSIONS"]
-
-        for key in all_keys:
-            if key in payload and key != "DIMENSIONS":
-                additional_info[key] = raw_data[key]
 
         return additional_info
 
