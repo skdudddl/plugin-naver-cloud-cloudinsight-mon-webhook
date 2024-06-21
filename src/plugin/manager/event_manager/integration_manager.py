492b03835154f12080d2b11f6c4086e566ed5036
skdudddl <knyg99@naver.com>
2024-06-21 23:31:28 +0900

    fix: change data's field

--- src/plugin/manager/event_manager/integration_manager.py
+++ src/plugin/manager/event_manager/integration_manager.py
@@ -20,16 +20,18 @@ class IntegrationManager(ParseManager):
         _LOGGER.debug(f"[CloudInsight] parse => {json.dumps(raw_data, indent=2)}")
 
         #event_type_category = raw_data.get("detail", {}).get("eventTypeCategory", "")
+        raw_data_text =raw_data.get("text")
+        # raw_data = self.parse_text_to_dict(raw_data_text)
 
         event: dict = {
             'event_key': self.generate_event_key(raw_data),
             'event_type': self.get_event_type(raw_data),
             'severity': self.get_severity(raw_data),
-            #'resource': self._get_resource(raw_data),#not required
+            'resource': raw_data.get("resourceName"),#not required
             'title': self._change_string_format(raw_data),
-            #'rule': raw_data.get("ruleName"),#not required
-            'description': raw_data.get("text"),
-            'occurred_at': self.convert_to_iso8601(raw_data.get("startTime")),
+            'rule': raw_data.get("ruleName"),#not required
+            'description': raw_data_text,
+            'occurred_at': self.convert_to_iso8601(raw_data.get("alarmStartTime")),
             'additional_info': self.get_additional_info(raw_data)
         }
 
@@ -41,7 +43,7 @@ class IntegrationManager(ParseManager):
         }
 
     def generate_event_key(self, raw_data: dict) -> str:
-        return raw_data.get("id")
+        return raw_data.get("eventId")
 
     def get_event_type(self, raw_data: dict) -> str:
         return raw_data.get("type")
@@ -59,18 +61,18 @@ class IntegrationManager(ParseManager):
     @staticmethod
     def get_additional_info(raw_data: dict) -> dict:
         additional_info = {
-            "url": raw_data.get("url")
+            "url": raw_data.get("url"),
+            "dimension": raw_data.get("dimension", {})
         }
 
         return additional_info
 
+    def parse_text_to_dict(self, text: str) -> dict:
+        raw_data_dict = {}
+        items = text.strip('{}').split(', ')
+        print(items)
+        for item in items:
+            key, value = item.split(": ", 1)
+            raw_data_dict[key.strip()] = value.strip()
+        return raw_data_dict
 
-    @staticmethod
-    def _get_resource(raw_data: dict) -> dict:
-        key = ["RESOURCE_NAME"]
-        if key in raw_data:
-            return {
-                    "name" : raw_data[key]
-            }
-        else:
-            return {"name": "Undefined"}
